0e21f53b08308a39639d4c938728740c82f48f7f
Jonathan55 <102492309+Jonathan5508@users.noreply.github.com>
2022-08-09 08:15:44 -0600

    Delete start.sh

--- start.sh
+++
@@ -1,6 +0,0 @@
-git add .
-git commit -m "first commit"
-#git branch -M main
-#git remote remove origin
-#git remote add origin https://github.com/lightningdisc/website-dist.git
-#git push -u origin main
