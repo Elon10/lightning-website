f4734ead3ca14f979a9a471ec12330b172650a35
Jonathan5508 <bb6445569@gmail.com>
2022-08-09 13:43:06 +0000

    Ok

---
+++ start.sh
@@ -0,0 +1,8 @@
+#git config --global user.email "bb6445569@gmail.com"
+#git config --global user.name "Jonathan5508"
+git add .
+#git remote remove origin
+git commit -m "Ok"
+git branch -M main
+git remote add origin https://github.com/lightningdisc/website-dist.git
+git push -u origin main
